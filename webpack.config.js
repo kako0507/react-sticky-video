3753eb2585c7eda4582763fc2db622624c999e96
Tim Kuo <kako0507@gmail.com>
2019-11-06 00:25:23 +0800

    chore: rename example bundle file to git commit hash for preventing gh-pages file cache

--- webpack.config.js
+++ webpack.config.js
@@ -4,29 +4,41 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const GitRevisionPlugin = require('git-revision-webpack-plugin');
 
-module.exports = (env, argv) => ({
-  entry: path.join(
-    __dirname,
-    argv.example
-      ? 'example/index.jsx'
-      : 'src/index.jsx',
-  ),
-  output: {
-    path: path.join(__dirname, argv.example ? 'build' : 'dist'),
-    filename: 'index.js',
-    ...(
-      argv.example
-        ? {}
-        : {
-          library: 'StickyVideo',
-          libraryTarget: 'umd',
-        }
-    ),
-  },
-  externals: argv.example
-    ? []
-    : [
+module.exports = (env, argv) => {
+  let entryFolder;
+  let output;
+  let css;
+  let externals;
+  let plugins;
+  if (argv.example) {
+    entryFolder = 'example';
+    output = {
+      path: path.join(__dirname, 'gh-pages'),
+      filename: 'index-[git-revision-hash].js',
+    };
+    css = 'css/index-[git-revision-hash].css';
+    externals = [];
+    plugins = [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'example/index.template.html'),
+        minify: {
+          removeComments: true,
+        },
+      }),
+      new GitRevisionPlugin(),
+    ];
+  } else {
+    entryFolder = 'src';
+    output = {
+      path: path.join(__dirname, 'dist'),
+      filename: 'index.js',
+      library: 'StickyVideo',
+      libraryTarget: 'umd',
+    };
+    css = 'index.css';
+    externals = [
       {
         react: {
           root: 'React',
@@ -35,100 +47,100 @@ module.exports = (env, argv) => ({
           amd: 'react',
         },
       },
-    ],
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        use: [
-          'babel-loader',
-          ...(argv.mode === 'production'
-            ? []
-            : ['webpack-strip-block']
-          ),
-        ],
-      },
-      {
-        test: /\.md$/,
-        use: 'raw-loader',
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          { loader: 'css-loader' },
-        ],
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              localsConvention: 'camelCase',
-              modules: {
-                mode: 'local',
-                localIdentName: 'sv-[name]-[local]-[hash:base64:5]',
+    ];
+    plugins = [];
+  }
+  return {
+    entry: path.join(
+      __dirname,
+      `${entryFolder}/index.jsx`,
+    ),
+    output,
+    externals,
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          use: [
+            'babel-loader',
+            ...(argv.mode === 'production'
+              ? []
+              : ['webpack-strip-block']
+            ),
+          ],
+        },
+        {
+          test: /\.md$/,
+          use: 'raw-loader',
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            { loader: 'css-loader' },
+          ],
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                localsConvention: 'camelCase',
+                modules: {
+                  mode: 'local',
+                  localIdentName: 'sv-[name]-[local]-[hash:base64:5]',
+                },
               },
             },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sassOptions: {
-                outputStyle: 'expanded',
+            {
+              loader: 'sass-loader',
+              options: {
+                sassOptions: {
+                  outputStyle: 'expanded',
+                },
               },
             },
-          },
-        ],
-      },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      ...plugins,
+      new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: css,
+      }),
+      new webpack.NormalModuleReplacementPlugin(/(.*)STICKY_VIDEO(\.*)/, ((resource) => {
+        // eslint-disable-next-line no-param-reassign
+        resource.request = resource.request.replace(
+          /STICKY_VIDEO/,
+          argv.mode === 'production'
+            ? 'dist'
+            : 'src',
+        );
+      })),
     ],
-  },
-  plugins: [
-    ...(argv.example
-      ? [
-        new HtmlWebpackPlugin({
-          template: path.join(__dirname, 'example/index.template.html'),
-          minify: {
-            removeComments: true,
+    stats: {
+      colors: true,
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    optimization: {
+      minimizer: [
+        new OptimizeCSSAssetsPlugin({
+          cssProcessorPluginOptions: {
+            preset: ['default', { discardComments: { removeAll: true } }],
           },
         }),
-      ]
-      : []
-    ),
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: 'index.css',
-    }),
-    new webpack.NormalModuleReplacementPlugin(/(.*)STICKY_VIDEO(\.*)/, ((resource) => {
-      // eslint-disable-next-line no-param-reassign
-      resource.request = resource.request.replace(
-        /STICKY_VIDEO/,
-        argv.mode === 'production'
-          ? 'dist'
-          : 'src',
-      );
-    })),
-  ],
-  stats: {
-    colors: true,
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  optimization: {
-    minimizer: [
-      new OptimizeCSSAssetsPlugin({
-        cssProcessorPluginOptions: {
-          preset: ['default', { discardComments: { removeAll: true } }],
-        },
-      }),
-      new TerserPlugin(),
-    ],
-  },
-  devServer: {
-    publicPath: '/build/',
-  },
-});
+        new TerserPlugin(),
+      ],
+    },
+    devServer: {
+      publicPath: '/gh-pages/',
+    },
+  };
+};
